25173b2febe3df9862bc5be22680d1a5136fc646
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2020-08-19 12:15:41 -0700

    Brute Force Approach to find solution

--- noReinforcementLearning.py
+++ noReinforcementLearning.py
@@ -1,3 +1,50 @@
 import gym
+from IPython.display import clear_output
+from time import sleep
 
-gym.make('Taxi-v3').env
+# brute force approach
+# takes near 5k epochs to complete
+
+env = gym.make('Taxi-v3').env
+
+# manually setting the starting state
+# env.s = 328
+
+epochs = 0
+penalties, reward = 0, 0
+
+frames = []
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+
+    if(reward == -10.0):
+        penalties += 1 
+
+    frames.append(
+        {
+            'frame': env.render(mode = 'ansi'), 
+            'state': state, 
+            'action': action, 
+            'reward': reward
+        }
+    )
+
+    epochs += 1
+
+print("Timesteps taken: {}".format(epochs))
+print("Penalties incurred: {}".format(penalties))
+
+def print_frames(frames):
+    for i, frame in enumerate(frames):
+        clear_output(wait=True)
+        print(frame['frame'])
+        print(f"Timestep: {i + 1}")
+        print(f"State: {frame['state']}")
+        print(f"Action: {frame['action']}")
+        print(f"Reward: {frame['reward']}")
+        sleep(.1)
+        
+print_frames(frames)
